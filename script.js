ad8bc8eb723d84b1dff14c7c2301b1b829f2367d
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-20 13:21:05 -0500

    Conditional statement addition to add leading zeros

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function getTime() {
 	let sec = dateTime.getSeconds(); //getSeconds() returns the seconds (0 to 59) of a date
 	let meridiem = document.getElementById("meridiem");
 
+	/* 
+	Using DOM to target meridiem ID to switch between 'AM' and 'PM' depending on hrs value
+	We want this to execute first before running conditional statement to use a 12 hour clock
+	*/
 	if(hrs >= 12) {
 		meridiem.innerHTML = "PM";
 	}
@@ -15,8 +19,22 @@ function getTime() {
 
 	// Conditional statement for 12 hour clock; comment out to show 24 hour clock if you want
 	if(hrs > 12) {
-		hrs = hrs - 12;
+		hrs -= 12
 	}
+
+	// Next 3 conditional statements add leading zeroes to hours, minutes, and seconds
+	if(hrs < 10) {
+		hrs = "0" + hrs;
+	}
+
+	if(min < 10) {
+		min = "0" + min;
+	}
+
+	if(sec < 10) {
+		sec = "0" + sec;
+	}
+
 
 	document.getElementById("hours").innerHTML = hrs;
 	document.getElementById("minutes").innerHTML = min;
